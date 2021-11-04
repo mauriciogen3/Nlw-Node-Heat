b85a4ebdace6c56a116fb314e57b4d7ff52c0ff8
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-11-03 22:00:14 -0300

    stage token

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,18 @@
+/*receber code(string)
+* recuperar o access_token no github
+* verificar se o usuario existe no DB
+------sim ----- = gera um token
+------nao------ = cria no db, gera um token
+retornar o token com as infos do user 
+
+45 mim stage 1 = https://app.rocketseat.com.br/node/mission-impulse-heat/lesson/stage-1-1
+*/
+
+class AuthenticateUserService{
+  async execute(code: string){
+
+  }
+
+}
+
+export{AuthenticateUserService};
